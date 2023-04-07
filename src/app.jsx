2ae9222b9e47cbad8c71edfe4153422a10bcec5a
Glitch (scarlet-sedate-kip) <none>
2023-04-07 02:43:24 +0000

    added React app for TLE

--- src/app.jsx
+++ src/app.jsx
@@ -1,52 +1,52 @@
-import React, { useState, useEffect } from "react";
-import { Router, Link } from "wouter";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
 
-/**
-* This code defines the react app
-*
-* Imports the router functionality to provide page navigation
-* Defines the Home function outlining the content on each page
-* Content specific to each page (Home and About) is defined in their components in /pages
-* Each page content is presented inside the overall structure defined here
-* The router attaches the page components to their paths
-*/
+const TLELine = ({ parts }) => (
+  <div className="tle">
+    {parts.map((part, index) => (
+      <React.Fragment key={index}>
+        <span data-tooltip={part.tooltip}>{part.text}</span>
+        {' '}
+      </React.Fragment>
+    ))}
+  </div>
+);
 
-// Import and apply CSS stylesheet
-import "./styles/styles.css";
+const App = () => {
+  const tle1_parts = [
+        { text: '1', tooltip: 'Line number (1 or 2)' },
+        { text: '25544', tooltip: 'Satellite catalog number' },
+        { text: 'U', tooltip: 'Classification (U for unclassified)' },
+        { text: '98067A', tooltip: 'International Designator (launch year, launch number, piece of launch)' },
+        { text: '21361.90679491', tooltip: 'Epoch (last two digits of year and day of the year with fractional days)' },
+        { text: '.00001338', tooltip: 'First time derivative of the mean motion (change in mean motion per day)' },
+        { text: '00000+0', tooltip: 'Second time derivative of the mean motion (decimal assumed)' },
+        { text: '62572-4', tooltip: 'BSTAR drag term (decimal assumed)' },
+        { text: '0', tooltip: 'Ephemeris type (0 for SGP4 model)' },
+        { text: '9999', tooltip: 'Element set number (incremented for updated TLE)' }
+    ];
 
-// Where all of our pages come from
-import PageRouter from "./components/router.jsx";
+  const tle2_parts = [
+        { text: '2', tooltip: 'Line number (1 or 2)' },
+        { text: '25544', tooltip: 'Satellite catalog number' },
+        { text: '51.6469', tooltip: 'Inclination (degrees)' },
+        { text: '358.2030', tooltip: 'Right ascension of the ascending node (degrees)' },
+        { text: '0008411', tooltip: 'Eccentricity (decimal assumed)' },
+        { text: '329.4627', tooltip: 'Argument of perigee (degrees)' },
+        { text: '30.6574', tooltip: 'Mean anomaly (degrees)' },
+        { text: '15.50067766', tooltip: 'Mean motion (revolutions per day)' },
+        { text: '358954', tooltip: 'Revolution number at epoch (revolutions)' }
+    ];
 
-// The component that adds our Meta tags to the page
-import Seo from './components/seo.jsx';
-
-// Home function that is reflected across the site
-export default function Home() {
   return (
-    <Router>
-      <Seo />
-      <main role="main" className="wrapper">
-        <div className="content">
-          {/* Router specifies which component to insert here as the main content */}
-          <PageRouter />
-        </div>
-      </main>
-      {/* Footer links to Home and About, Link elements matched in router.jsx */}
-      <footer className="footer">
-        <div className="links">
-          <Link href="/">Home</Link>
-          <span className="divider">|</span>
-          <Link href="/about">About</Link>
-        </div>
-        <a
-          className="btn--remix"
-          target="_top"
-          href="https://glitch.com/edit/#!/remix/glitch-hello-react"
-        >
-          <img src="https://cdn.glitch.com/605e2a51-d45f-4d87-a285-9410ad350515%2FLogo_Color.svg?v=1618199565140" alt="" />
-          Remix on Glitch
-        </a>
-      </footer>
-    </Router>
+    <div className="App">
+      <div className="tle-container">
+        <TLELine parts={tle1_parts} />
+        <TLELine parts={tle2_parts} />
+      </div>
+    </div>
   );
-}
+};
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./app.jsx";
-import { HelmetProvider } from 'react-helmet-async';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './app';
+import './app.css';
 
-/**
-* Root of react site 
-*
-* Imports Helment provider for the page head
-* And App which defines the content and navigation
-*/
-
-// Render the site https://reactjs.org/docs/react-dom.html#render
 ReactDOM.render(
   <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
+    <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
